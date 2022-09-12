4f184755535ed327d4bd2d59d22c6a8421791f07
krupenmax <krupendeu@gmail.com>
2022-09-12 17:28:59 +0300

    First commit

--- src/app/pullcompany.service.ts
+++ src/app/pullcompany.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root',
 })
 export class PullcompanyService {
-  public mes = this.http.get<{}[]>('http://www.portal.nalog.gov.by/grp/getData?unp=%7BUNP%7D&charset=UTF-8&type=json');
 
-  subscribe() {
-    this.mes.subscribe();
-    console.log(this.mes);
+  private mes = this.http.get<{}[]>('http://www.portal.nalog.gov.by/grp/getData?unp=%7BUNP%7D&charset=UTF-8&type=json');
+  
+  public subscribe() {
+    this.mes.subscribe(data=>{
+      console.log(data)});     
   }
 
   constructor(private http: HttpClient) {
